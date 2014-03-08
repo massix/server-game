48043af420fb818a3b0a8cf917b7ab3680eb86ad
dchilot <dchilot@hotmail.com>
2014-03-08 20:30:50 +0000

    Use a global logger. Extract UDP broadcasting from server in a separate class. Try to simplify the test for the UDP broadcast.

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,19 @@
 #include "orwell/Server.hpp"
+#include "orwell/BroadcastServer.hpp"
 
-#include <log4cxx/logger.h>
-#include <log4cxx/patternlayout.h>
-#include <log4cxx/consoleappender.h>
-#include <log4cxx/fileappender.h>
-#include <log4cxx/basicconfigurator.h>
-#include <log4cxx/helpers/exception.h>
-#include <log4cxx/filter/levelrangefilter.h>
-#include <log4cxx/ndc.h>
+#include <zmq.hpp>
 
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <stdio.h>
 #include <signal.h>
 
+#include "orwell/support/GlobalLogger.hpp"
+
 using namespace log4cxx;
 using namespace std;
 
-static orwell::tasks::Server * ServerPtr;
+static orwell::Server * ServerPtr;
 static void signal_handler(int signum)
 {
 	ServerPtr->stop();
@@ -27,18 +23,12 @@ int main()
 {
 	std::string aString;
 
-	PatternLayoutPtr aPatternLayout = new PatternLayout("%d %-5p %x (%F:%L) - %m%n");
-	ConsoleAppenderPtr aConsoleAppender = new ConsoleAppender(aPatternLayout);
-	filter::LevelRangeFilterPtr aLevelFilter = new filter::LevelRangeFilter();
-	aLevelFilter->setLevelMin(Level::getInfo());
-	aConsoleAppender->addFilter(aLevelFilter);
-	FileAppenderPtr aFileApender = new FileAppender( aPatternLayout, "orwelllog.txt");
-	BasicConfigurator::configure(aFileApender);
-	BasicConfigurator::configure(aConsoleAppender);
-	log4cxx::LoggerPtr logger(log4cxx::Logger::getLogger("orwell.log"));
-	logger->setLevel(log4cxx::Level::getDebug());
+	orwell::support::GlobalLogger("server_web", "orwell.log", true);
 
-	orwell::tasks::Server aServer("tcp://*:9000", "tcp://*:9001", 500, logger);
+	zmq::context_t aContext(1);
+	std::string const aPullerUrl("tcp://*:9000");
+	std::string const aPublisherUrl("tcp://*:9001");
+	orwell::Server aServer(aContext, aPullerUrl, aPublisherUrl, 500);
 	aServer.accessContext().addRobot("Gipsy Danger");
 	aServer.accessContext().addRobot("Goldorak");
 	aServer.accessContext().addRobot("Securitron");
@@ -54,25 +44,31 @@ int main()
 	switch (aChildProcess)
 	{
 		case 0:
-			LOG4CXX_INFO(logger, "Child started, pid: " << aChildProcess);
-			aServer.runBroadcastReceiver();
+		{
+			ORWELL_LOG_INFO("Child started, pid: " << aChildProcess);
+			orwell::BroadcastServer aBroadcastServer(aPullerUrl, aPublisherUrl);
+			aBroadcastServer.runBroadcastReceiver();
 			
 			// The child can stop here
 			return 0;
-			break;
+		}
 		default:
-			LOG4CXX_INFO(logger, "Father continued, pid: " << aChildProcess);
+		{
+			ORWELL_LOG_INFO("Father continued, pid: " << aChildProcess);
 			aServer.loop();
 			break;
+		}
 	}
 	
 	// Let's wait for everything to be over.
 	int status;
 	while (waitpid(aChildProcess, &status, WNOHANG) == 0)
 	{
-		LOG4CXX_INFO(logger, "Waiting for child " << aChildProcess << " to complete..");
+		ORWELL_LOG_INFO("Waiting for child " << aChildProcess << " to complete..");
 		sleep(1);
 	}
 
+	orwell::support::GlobalLogger::Clear();
 	return 0;
 }
+
